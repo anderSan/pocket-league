9a8fd81d4839e8190f6ff80e3516ae8ee145462a
phillip <anderson.pa@gmail.com>
2015-01-27 21:14:07 +0000

    Added attributions for gametype icons.

--- app/src/main/java/com/twobits/pocketleague/AboutPage.java
+++ app/src/main/java/com/twobits/pocketleague/AboutPage.java
@@ -3,11 +3,15 @@ package com.twobits.pocketleague;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.twobits.pocketleague.db.OrmLiteFragment;
+
+import org.w3c.dom.Text;
 
 public class AboutPage extends OrmLiteFragment {
 	private View rootView;
@@ -23,6 +27,9 @@ public class AboutPage extends OrmLiteFragment {
 			Bundle savedInstanceState) {
 		rootView = inflater.inflate(R.layout.fragment_about_page, container,
 				false);
+
+        TextView tv_flaticon = (TextView) rootView.findViewById(R.id.tv_flaticon_acknowledgment);
+        tv_flaticon.setMovementMethod(LinkMovementMethod.getInstance());
 
 		return rootView;
 	}
