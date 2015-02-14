a9299cb71b76ec8931cb183bee3e22432b7a25ca
phillip <anderson.pa@gmail.com>
2015-02-14 17:20:25 +0000

    Imported old PolishHorseshoes code. Fixed imports within code but this is far from working.

---
+++ polishhorseshoes/src/main/java/com/twobits/polishhorseshoes/Activity_Base.java
@@ -0,0 +1,34 @@
+package com.twobits.polishhorseshoes;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.MenuItem;
+
+public class Activity_Base extends ActionBarActivity {
+    protected String LOGTAG = getClass().getSimpleName();
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public void log(String msg) {
+		Log.i(LOGTAG, msg);
+	}
+
+	public void logd(String msg) {
+		Log.d(LOGTAG, msg);
+	}
+
+	public void loge(String msg, Exception e) {
+		Log.e(LOGTAG, msg + ": " + e.getMessage());
+	}
+}
---
+++ polishhorseshoes/src/main/java/com/twobits/polishhorseshoes/rulesets/RuleSet02.java
@@ -0,0 +1,41 @@
+package com.twobits.polishhorseshoes.rulesets;
+
+import com.twobits.polishhorseshoes.db.Throw;
+import com.twobits.polishhorseshoes.enums.ThrowType;
+
+public class RuleSet02 extends RuleSet01 {
+	@Override
+	public int getId() {
+		return 2;
+	}
+	@Override
+	public String getDescription() {
+		return "Bottle hit scores 1 like drop; stalwart can't win.";
+	}
+	private boolean isLegalBottleHit(Throw t){
+		boolean isHit = (t.throwType==ThrowType.BOTTLE && 
+				!t.isLineFault && 
+				!t.isGoaltend && 
+				!isOffenseOnHill(t));
+		return isHit;
+	}
+	
+	@Override
+	protected void handleCatch(Throw t, int[] diffs) {
+		super.handleCatch(t, diffs);
+		if (isLegalBottleHit(t)){
+			diffs[0] = 1;
+		}
+	}
+	
+	@Override
+	protected void handleStalwart(Throw t, int[] diffs) {
+		super.handleStalwart(t, diffs);
+		if (isDefenseOnHill(t)){
+			diffs[1] = 0;
+		} 
+		if (isLegalBottleHit(t)){
+			diffs[0] = 1;
+		}
+	}
+}
