4d38b15de60ed5bd25eb851ba44fd3dcbf9013d4
phillip <anderson.pa@gmail.com>
2015-05-01 00:40:07 +0100

    Added stub for Ladder sessions

--- app/src/main/java/com/twobits/pocketleague/Detail_Session_Ladder.java
+++ app/src/main/java/com/twobits/pocketleague/Detail_Session_Ladder.java
@@ -1,19 +1,39 @@
 package com.twobits.pocketleague;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.twobits.pocketleague.backend.Detail_Session_Base;
+import com.twobits.pocketleague.backend.LadderTable;
 
 public class Detail_Session_Ladder extends Detail_Session_Base {
+    private LadderTable ladder_table;
 
     @Override
     public void createSessionLayout(LayoutInflater inflater, ViewGroup container) {
         rootView = inflater.inflate(R.layout.fragment_detail_session_ladder, container, false);
+        FrameLayout fl = (FrameLayout) rootView.findViewById(R.id.frame1);
+
+        ladder_table = new LadderTable(fl, s) {
+            @Override
+            public void onClick(View v) {
+                mInfo = getMatch((int) v.getTag());
+                log("gId: " + mInfo.getIdInSession() + ", create: "
+                        + mInfo.getCreatable() + ", view: "
+                        + mInfo.getViewable() + ", marquee: " + mInfo.getTitle()
+                        + ", " + mInfo.getSubtitle());
+                mActionMode = rootView.startActionMode(new ActionBarCallBack());
+                v.setSelected(true);
+            }
+        };
     }
 
     @Override
     public void refreshDetails() {
-
+        if (ladder_table != null) {
+            ladder_table.refreshTable();
+        }
     }
 }
